21a1d71c5b4a7ff13df7f93aff3f02cfb7d84c4f
Kobakhit <kobakhit@gmail.com>
2018-08-02 13:56:53 -0400

    fix linenumbers

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(function(){
 	// add line-number to <pre> and wrapt in <table>
 	// http://mrloh.se/2015/05/bending-markdown-for-jekyll-and-github-pages/#fnref1
-	// var table = '<table class="highlighttable"><tbody><tr></tr></tbody></table>';
-	// $('article .highlight').wrap(table).before(function() {
-	// 	if($(this).parents(".gist").length === 0){
-	// 		// do not add line numbers to jekyll gists
-	// 	    var out = '<td class="linenos"><div class="linenodiv"><pre><code>';
-	// 	    var lines = $(this).text().split(/\n/).length;
-	// 	    for ( var i=1; i<lines; i++ ) {
-	// 	        out += i+'\n';
-	// 	    }
-	// 	    return out + '</code></pre></div></td>';
-	// 	}
-	// }).wrap('<td class="code"></td>');
+	var table = '<table class="highlighttable"><tbody><tr></tr></tbody></table>';
+	$('article pre.highlight').wrap(table).before(function() {
+		if($(this).parents(".gist").length === 0){
+			// do not add line numbers to jekyll gists
+		    var out = '<td class="linenos"><div class="linenodiv"><pre><code>';
+		    var lines = $(this).text().split(/\n/).length;
+		    for ( var i=1; i<lines; i++ ) {
+		        out += i+'\n';
+		    }
+		    return out + '</code></pre></div></td>';
+		}
+	}).wrap('<td class="code"></td>');
 
 	// white background for output code chunks
 	$("code.language-out").parent().wrap( "<div class='outtable'></div>")
